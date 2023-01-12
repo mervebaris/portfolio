c44aea75e1152baf691ce6ed72ec71b2e0e5ccf5
Merve Barış <99658291+mervebaris@users.noreply.github.com>
2023-01-12 18:00:27 +0300

    init

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,14 +2,14 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className="h-56 bg-red pt-16 ">
+    <footer className="h-56 bg-red pt-8 ">
       <div className="w-10/12 mx-auto">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
             MERVE BARIŞ
           </p>
-          <p className="font-playfair text-md text-yellow">
+          <p className="font-playfair text-xl text-yellow">
             ©2023 ESPER. All Rights Reserved.
           </p>
         </div>
